68efc9bfab7efb86684ffbb59fff6e2c9732a6a4
Ha Hai Cuong <cuonghhse181521@fpt.edu.vn>
2025-06-20 00:53:34 +0700

    9. Adjust AdminDashboard - try add avatar

--- src/components/admin/AdminDashboard.jsx
+++ src/components/admin/AdminDashboard.jsx
@@ -13,50 +13,77 @@ import NavbarData from './NavbarData';
 import NavbarTheme from './navbar-theme';
 
 function DemoPageContent({ pathname }) {
-    return (
-        <Box
-            sx={{
-                py: 4,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                textAlign: "center",
-            }}
-        >
-            <Typography>Dashboard content for {pathname}</Typography>
-        </Box>
-    );
+  return (
+    <Box
+      sx={{
+        py: 4,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        textAlign: "center",
+      }}
+    >
+      <Typography>Dashboard content for {pathname}</Typography>
+    </Box>
+  );
 }
 
 DemoPageContent.propTypes = {
-    pathname: PropTypes.string.isRequired,
+  pathname: PropTypes.string.isRequired,
 };
 
 function DashboardLayoutBasic(props) {
-    const { window } = props;
+  const { window } = props;
 
-    const router = useDemoRouter("/dashboard");
+  const [session, setSession] = React.useState({
+    user: {
+      name: 'Bharat Kashyap',
+      email: 'bharatkashyap@outlook.com',
+      image: 'https://avatars.githubusercontent.com/u/19550456',
+    },
+  });
 
-    // Remove this const when copying and pasting into your project.
-    const demoWindow = window !== undefined ? window() : undefined;
+  const authentication = React.useMemo(() => {
+    return {
+      signIn: () => {
+        setSession({
+          user: {
+            name: 'Bharat Kashyap',
+            email: 'bharatkashyap@outlook.com',
+            image: 'https://avatars.githubusercontent.com/u/19550456',
+          },
+        });
+      },
+      signOut: () => {
+        setSession(null);
+      },
+    };
+  }, []);
+
+  const router = useDemoRouter("/dashboard");
+
+  // Remove this const when copying and pasting into your project.
+  const demoWindow = window !== undefined ? window() : undefined;
 
   return (
     // Remove this provider when copying and pasting into your project.
     <DemoProvider window={demoWindow}>
       {/* preview-start */}
       <AppProvider
+        session={session}
+        authentication={authentication}
         navigation={NavbarData}
         router={router}
         theme={NavbarTheme}
         window={demoWindow}
         branding={{
-          logo: <img  src={LogoBranchImg} 
-                      alt="School Medical logo" 
-                      style={{ 
-                        // the width and height are fixed in their parent div, can over the parent's size
-                        height: '40px', 
-                        width: 'auto', 
-                      }}
+          logo: <img src={LogoBranchImg}
+            alt="School Medical logo"
+            style={{
+              // the width and height are fixed in their parent div, can over the parent's size
+              height: '40px',
+              width: 'auto',
+            }}
           />,
           title: '',
           homeUrl: '/homepage',
