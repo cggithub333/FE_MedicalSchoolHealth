2f0f0d701404afa7c9146c79f8d4acbf1442fe6b
Ha Hai Cuong <cuonghhse181521@fpt.edu.vn>
2025-06-23 02:43:06 +0700

    36. Configure TypeScript in

---
+++ src/components/magic/ScheduleTemplate/ScheduleTemplate.jsx
@@ -0,0 +1,13 @@
+
+import './StyleScheduleTemplate.module.css';
+
+const ScheduleTemplate = () => {
+
+  return (
+    <>
+      Schedule Template
+    </>
+  );
+}
+
+export default ScheduleTemplate;
--- src/pages/parent/health-check-campaign/Schedule.jsx
+++ src/pages/parent/health-check-campaign/Schedule.jsx
@@ -1,7 +1,14 @@
+import ScheduleTemplate from "../../../components/magic/ScheduleTemplate/ScheduleTemplate";
+
+
 const Schedule = () => {
   return (
     <>
       Role: Parent - Folder: health-check-campaign - File: Schedule
+
+      <div style={{ margin: "30px 20px" }}>
+        <ScheduleTemplate />
+      </div>
     </>
   )
 }
