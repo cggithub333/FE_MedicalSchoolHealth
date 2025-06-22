98d87104076df0d10d5202e3aa8e61d823d35d11
CHU THANH TINHend <tinhctse181528@fpt.edu.vn>
2025-06-22 07:28:59 +0700

    13. init base data campaign for manager

--- src/api/manager/managet-requests/parentRequests.js
+++
@@ -1,9 +0,0 @@
-
-
-import Request from "../Request";
-
-export const requestAllPupils = async () =>  Request.get('pupils');
-
-export const requestAllVaccinationHistoryByPupilId = async (pupilId) => Request.get(`vaccination-history/?pupilId=${pupilId}`);
-
-export const requestAllVaccinationSurveyByPupilId = async (pupilId) => Request.get(`vaccination-survey/?pupilId=${pupilId}`);
