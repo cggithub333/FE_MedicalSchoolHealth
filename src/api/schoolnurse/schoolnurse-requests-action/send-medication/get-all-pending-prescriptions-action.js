2aa7e677950f6c8f30d7dee4f64f794050079264
Ha Hai Cuong <cuonghhse181521@fpt.edu.vn>
2025-07-03 20:25:35 +0700

    83. School nurse - Prescription Request List

---
+++ src/api/schoolnurse/schoolnurse-requests-action/send-medication/get-all-pending-prescriptions-action.js
@@ -0,0 +1,22 @@
+
+import {getAllPendingPrescriptionsCallback} from '@api/schoolnurse/schoolnurse-requests-callback/send-medication/get-all-pending-prescriptions-callback'
+import { fetchResponse } from '@api/fetch-response'
+
+export const getAllPendingPrescriptionsAction = async () => {
+
+  try {
+    const callback = () => getAllPendingPrescriptionsCallback();
+
+    const response = await fetchResponse(callback);
+
+    // debug:
+    console.log("getAllPendingPrescriptionsAction response: ", response);
+
+    const pendingMedicationRequests = await response.data || []; 
+    return pendingMedicationRequests
+
+  } catch(error) {
+    console.error("Error in getAllPendingPrescriptionsAction: ", error);
+    throw error; // Re-throw the error to be handled by the caller
+  }
+}
