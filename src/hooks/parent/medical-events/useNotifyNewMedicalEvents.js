d3f25174065504af826952060be39e198fd67c8a
Ha Hai Cuong <cuonghhse181521@fpt.edu.vn>
2025-07-05 14:24:05 +0700

    102. Notification for Medical Events

---
+++ src/hooks/parent/medical-events/useNotifyNewMedicalEvents.js
@@ -0,0 +1,34 @@
+
+//getNofityNewMedicalEventsCallback
+
+import { useState, useEffect, useCallback } from "react";
+import { getNofityNewMedicalEventsAction } from "@api/parent/parent-requests-action/medical-events/get-notify-new-medical-events-action.js";
+
+const useNotifyNewMedicalEvents = () => {
+  const [notiNewEventOfPupils, setNotiNewEventOfPupils] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchNotiNewEventOfPupils = useCallback(async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await getNofityNewMedicalEventsAction();
+      //debug:
+      console.log("Fetched Notifications for Pupils' New Events:", response);
+      setNotiNewEventOfPupils(response.data);
+    } catch (err) {
+      setError(err.message);
+      console.error("Error fetching notifications for pupils' new events:", err);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+  useEffect(() => {
+    fetchNotiNewEventOfPupils();
+  }, [fetchNotiNewEventOfPupils]);
+
+  return { notiNewEventOfPupils, loading, error, refetch: fetchNotiNewEventOfPupils };
+}
+
+export default useNotifyNewMedicalEvents;
--- src/pages/parent/health-management/MedicalEvents.jsx
+++ src/pages/parent/health-management/MedicalEvents.jsx
@@ -1,9 +1,40 @@
+
+import MedicalEventsContent from "@components/parent/HealthManagement/MedicalEventsContent";
+
+import { Grid } from "@mui/material";
+import CustomTittle from "@components/magic/CustomTittle/CustomTitle";
+import Breadcrumb from "@components/magic/Breadcrumb/CustomBreadcrumb";
+
 const MedicalEvents = () => {
   return (
-    <>
-      Role: Parent - Folder: health-management - File: MedicalEvents
-    </>
+    <div style={{ background: "#E6F8F9", height: "100vh", paddingBottom: "50px", paddingTop: "10px" }}>
+      <Grid container>
+        <Grid item size={{ xs: 6 }}>
+          <Breadcrumb breadcrumbPairs={breadcrumbPairs} />
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item sx={{ marginLeft: "20px", marginTop: "25px" }} size={{ xs: 6 }}>
+          <CustomTittle title={"Medical Events"} />
+        </Grid>
+      </Grid>
+      <Grid container justifyContent={'center'} sx={{ marginTop: "20px", backgroundColor: "#E6F8F9", pb: "50px" }}>
+        <Grid item size={{ xs: 10 }}>
+          <MedicalEventsContent />
+        </Grid>
+      </Grid>
+    </div>
   )
 }
+
+const breadcrumbPairs = [
+  {
+    title: 'Dashboard',
+    link: '/parent/dashboard'
+  },
+  {
+    title: 'Medical Events',
+  }
+]
 
 export default MedicalEvents;
