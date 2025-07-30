0e7a9bcae23338ffd4d006a7345cedfa7d8183f2
Ha Hai Cuong <hhc9104@gmail.com>
2025-07-31 00:34:44 +0700

    208. Make blog content show the right <div>, <b>, <i>, <u>,..

--- src/components/blog-resources/blog-card.jsx
+++ src/components/blog-resources/blog-card.jsx
@@ -8,7 +8,21 @@ import {
 import { Person, Verified } from "@mui/icons-material"
 import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh';
 
+import { useNavigate } from "react-router-dom";
+import { showErrorToast } from "@utils/toast-utils";
+
 const BlogCard = ({ blog, height = "500px", isInAdminPower = false, isRelatedBlogs = false, handleNavigateDetail }) => {
+
+  const navigate = useNavigate();
+
+  const handleNavigateEditPage = () => {
+    if (!blog || !blog.blogId) {
+      showErrorToast("Edit is not available for this blog. Choose another blog and continue editing.");
+      return;
+    }
+    // else:
+    navigate(`/blogs/manage/edit/${blog.blogId}`);
+  }
 
   return (
     <Card sx={styleCard(isInAdminPower, isRelatedBlogs)}>
@@ -22,10 +36,17 @@ const BlogCard = ({ blog, height = "500px", isInAdminPower = false, isRelatedBlo
         <Typography gutterBottom variant="h5" component="div" sx={{ color: 'text.secondary', fontSize: isRelatedBlogs ? "1.2rem" : "2rem" }}>
           {blog?.title || "Medical Blog"}
         </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          {!isRelatedBlogs && <>{blog?.content}</>}
-          {isRelatedBlogs && <>{blog?.content.split(/\s+/).filter((_, idx) => idx < 25).join(' ')}...</>}
-        </Typography>
+        <Typography
+          variant="body2"
+          sx={{ color: 'text.secondary' }}
+          component="div"
+          dangerouslySetInnerHTML={{
+            __html: isRelatedBlogs
+              ? blog?.content.split(/\s+/).slice(0, 25).join(' ') + '...'
+              : blog?.content,
+          }}
+        />
+
       </CardContent>
       {!isRelatedBlogs && (
         <Stack direction="row" spacing={2} alignItems="center" justifyContent={'space-between'} sx={{ mt: 2, padding: 3 }}>
@@ -48,7 +69,7 @@ const BlogCard = ({ blog, height = "500px", isInAdminPower = false, isRelatedBlo
           </Stack>
 
           {isInAdminPower && (
-            <Stack component={Button} direction={'row'} spacing={1} alignItems="center">
+            <Stack component={Button} direction={'row'} spacing={1} alignItems="center" onClick={handleNavigateEditPage}>
               <Avatar sx={{ width: 32, height: 32, bgcolor: "#56a8ed" }}>
                 <AutoFixHighIcon fontSize="small" />
               </Avatar>
